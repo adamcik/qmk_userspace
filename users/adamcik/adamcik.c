7de294583b4f9af6a1aea73c8e2b7d0d6645e369
Thomas Adamcik <thomas@adamcik.no>
2024-10-24 12:57:54 +0200

    Minor cleanups

--- users/adamcik/adamcik.c
+++ users/adamcik/adamcik.c
@@ -3,72 +3,44 @@
 #include "oneshot.h"
 #include "definitions.h"
 
-#ifndef POINTING_DEVICE_ENABLE
-#define DRGSCRL KC_NO
-#define DPI_MOD KC_NO
-#define S_D_MOD KC_NO
-#define SNIPING KC_NO
-#endif // !POINTING_DEVICE_ENABLE
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [COL] = LAYOUT_adamcik(
-        KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_QUOT,
-        KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,
-        KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SCLN,
-                          LA_PTR,  LA_NAV,  KC_SPC,  KC_LSFT, LA_SYM,  XXXXXXX
+        KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,        KC_J,    KC_L,    KC_U,    KC_Y,    KC_QUOT,
+        KC_A,    KC_R,    KC_S,    KC_T,    KC_G,        KC_M,    KC_N,    KC_E,    KC_I,    KC_O,
+        KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,        KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SCLN,
+                          LA_PTR,  LA_NAV,  KC_SPC,      KC_LSFT, LA_SYM,  XXXXXXX
     ),
     [QWE] = LAYOUT_adamcik(
-        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT,
-        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                          LA_PTR,  LA_NAV,  KC_SPC,  KC_LSFT, LA_SYM,  XXXXXXX
+        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT,
+        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
+                          LA_PTR,  LA_NAV,  KC_SPC,      KC_LSFT, LA_SYM,  XXXXXXX
     ),
     [SYM] = LAYOUT_adamcik(
-        KC_ESC,  KC_LBRC, KC_LCBR, KC_LPRN, KC_TILD, KC_CIRC, KC_RPRN, KC_RCBR, KC_RBRC, KC_GRV,
-        KC_MINS, KC_ASTR, KC_EQL,  KC_UNDS, KC_DLR,  OS_ALTG, OS_CMD,  OS_ALT,  OS_CTRL, OS_SHFT,
-        KC_PLUS, KC_PIPE, KC_AT,   KC_BSLS, KC_PERC, KC_HASH, KC_SLSH, KC_AMPR, KC_QUES, KC_EXLM,
-                          _______, _______, _______, _______, _______, _______
+        KC_ESC,  KC_LBRC, KC_LCBR, KC_LPRN, KC_TILD,     KC_CIRC, KC_RPRN, KC_RCBR, KC_RBRC, KC_GRV,
+        KC_MINS, KC_ASTR, KC_EQL,  KC_UNDS, KC_DLR,      OS_ALTG, OS_CMD,  OS_ALT,  OS_CTRL, OS_SHFT,
+        KC_PLUS, KC_PIPE, KC_AT,   KC_BSLS, KC_PERC,     KC_HASH, KC_SLSH, KC_AMPR, KC_QUES, KC_EXLM,
+                          _______, _______, _______,     _______, _______, _______
     ),
     [NAV] = LAYOUT_adamcik(
-        KC_TAB,  XXXXXXX, KC_VOLU, KC_MUTE, KC_VOLD, KC_BRIU, KC_HOME, KC_UP,   KC_END,  KC_DEL,
-        OS_SHFT, OS_CTRL, OS_ALT,  OS_CMD,  OS_ALTG, TG(QWE), KC_LEFT, KC_DOWN, KC_RGHT, KC_BSPC,
-        QK_BOOT, KC_MRWD, KC_MSTP, KC_MPLY, KC_MNXT, KC_BRID, KC_PGDN, KC_INS,  KC_PGUP, KC_ENT,
-                          _______, _______, _______, _______, _______, _______
+        KC_TAB,  KC_VOLU, KC_MUTE, KC_VOLD, QK_BOOT,     KC_BRIU, KC_HOME, KC_UP,   KC_END,  KC_DEL,
+        OS_SHFT, OS_CTRL, OS_ALT,  OS_CMD,  OS_ALTG,     TG(QWE), KC_LEFT, KC_DOWN, KC_RGHT, KC_BSPC,
+        KC_MRWD, KC_MSTP, KC_MPLY, KC_MNXT, EE_CLR,      KC_BRID, KC_PGDN, KC_INS,  KC_PGUP, KC_ENT,
+                          _______, _______, _______,     _______, _______, _______
     ),
     [NUM] = LAYOUT_adamcik(
-        KC_7,    KC_5,    KC_3,    KC_1,    KC_9,    KC_8,    KC_0,    KC_2,    KC_4,    KC_6,
-        OS_SHFT, OS_CTRL, OS_ALT,  OS_CMD,  KC_F11,  KC_F12,  OS_CMD,  OS_ALT,  OS_CTRL, OS_SHFT,
-        KC_F7,   KC_F5,   KC_F3,   KC_F1,   KC_F9,   KC_F8,   KC_F10,  KC_F2,   KC_F4,   KC_F6,
-                          _______, _______, _______, _______, _______, _______
+        KC_7,    KC_5,    KC_3,    KC_1,    KC_9,        KC_8,    KC_0,    KC_2,    KC_4,    KC_6,
+        OS_SHFT, OS_CTRL, OS_ALT,  OS_CMD,  KC_F11,      KC_F12,  OS_CMD,  OS_ALT,  OS_CTRL, OS_SHFT,
+        KC_F7,   KC_F5,   KC_F3,   KC_F1,   KC_F9,       KC_F8,   KC_F10,  KC_F2,   KC_F4,   KC_F6,
+                          _______, _______, _______,     _______, _______, _______
     ),
     [PTR] = LAYOUT_adamcik(
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_BOOT,
-        KC_BTN2, KC_BTN1, KC_BTN3, DRGSCRL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, DPI_MOD, SNIPING, S_D_MOD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, EE_CLR,
-                          KC_BTN2, KC_BTN1, KC_BTN3, XXXXXXX, XXXXXXX, XXXXXXX
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, DPI_MOD,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        KC_BTN2, KC_BTN1, KC_BTN3, DRGSCRL, SNIPING,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, S_D_MOD,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                          XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX
     )
 };
-
-
-/*
-const key_override_t next_track_override =
-    ko_make_with_layers_negmods_and_options(
-        MOD_MASK_CTRL, KC_MPLY, KC_MNXT, ~0, MOD_MASK_SA,
-        ko_option_no_reregister_trigger);
-
-const key_override_t prev_track_override =
-    ko_make_with_layers_negmods_and_options(
-        MOD_MASK_CS, KC_MPLY, KC_MPRV, ~0, MOD_MASK_SA,
-        ko_option_no_reregister_trigger);
-
-const key_override_t **key_overrides = (const key_override_t *[]){
-    &next_track_override,  // Ctrl + play/pause         --> next track
-    &prev_track_override,  // Ctrl + shift + play/pause --> prev track
-    NULL,
-};
-
-bool sw_alt_active = false;
-*/
 
 oneshot_state os_shft_state = os_up_unqueued;
 oneshot_state os_ctrl_state = os_up_unqueued;
